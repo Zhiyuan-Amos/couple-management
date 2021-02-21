86fab6e673fd576afda6b8f8e02b51a924a59999
Goh Yong Kiat <yongkiat92@gmail.com>
2021-02-21 20:04:57 +0800

    Initialize fullCalendar

--- Client/Pages/Calendar/Calendar.razor.cs
+++ Client/Pages/Calendar/Calendar.razor.cs
@@ -7,24 +7,33 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
+<<<<<<< HEAD
+=======
+using System.Linq;
+>>>>>>> 3ff24a5... Initialize fullCalendar
 using System.Threading.Tasks;
 
 namespace Couple.Client.Pages.Calendar
 {
     public partial class Calendar
     {
+        private ElementReference fullCalendar;
+
         [Inject]
         private NavigationManager NavigationManager { get; init; }
 
         [Inject]
         private EventStateContainer EventStateContainer { get; init; }
 
+<<<<<<< HEAD
         [Inject]
         private IMapper Mapper { get; init; }
 
         [Inject]
         private IJSRuntime Js { get; init; }
 
+=======
+>>>>>>> 3ff24a5... Initialize fullCalendar
         [Parameter]
         public DateTime Selected { get; set; }
 
@@ -42,8 +51,24 @@ namespace Couple.Client.Pages.Calendar
 
             EventStateContainer.OnChange += StateHasChanged;
 
+<<<<<<< HEAD
             var events = await Js.InvokeAsync<List<EventModel>>("getAllEvents");
             EventStateContainer.SetEvents(events);
+=======
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JS.InvokeVoidAsync(
+                    "intializeCalendar", fullCalendar);
+            }
+        }
+
+        protected void DateChangedHandler(DateTime newDateValue)
+        {
+            NavigationManager.NavigateTo($"/calendar/{newDateValue.ToCalendarUrl()}");
+            Events = GetEvents(newDateValue);
+>>>>>>> 3ff24a5... Initialize fullCalendar
         }
 
         public void Dispose() => EventStateContainer.OnChange -= StateHasChanged;
@@ -52,7 +77,16 @@ namespace Couple.Client.Pages.Calendar
 
         protected void ValueChangedHandler(DateTime newDateValue) => NavigationManager.NavigateTo($"/calendar/{newDateValue.ToCalendarUrl()}");
 
+<<<<<<< HEAD
         protected void AddEvent() => NavigationManager.NavigateTo($"/calendar/create");
         protected void EditEvent(EventViewModel selectedEvent) => NavigationManager.NavigateTo($"/calendar/{selectedEvent.Id}");
+=======
+        protected void RefreshEvents() => Events = GetEvents(Selected);
+
+        private async Task GetCalendar()
+        {
+            await JS.InvokeVoidAsync("intializeCalendar");
+        }
+>>>>>>> 3ff24a5... Initialize fullCalendar
     }
 }
