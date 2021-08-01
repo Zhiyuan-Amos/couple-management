8c599ea70e817f4d46f56b0bc6a653cfb9c4a369
Yong Zhi Yuan <yongzhiyuan@u.nus.edu>
2021-08-01 18:33:14 +0800

    ToDo: Add logic for checkboxes

--- Client/Pages/ToDo/Components/ToDoListView.razor.cs
+++ Client/Pages/ToDo/Components/ToDoListView.razor.cs
@@ -1,4 +1,6 @@
+using Couple.Client.Adapters;
 using Couple.Client.Model.ToDo;
+using Couple.Client.States.ToDo;
 using Couple.Client.ViewModel.ToDo;
 using Couple.Shared.Model;
 using Microsoft.AspNetCore.Components;
@@ -6,12 +8,16 @@ using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace Couple.Client.Pages.ToDo.Components
 {
     public partial class ToDoListView
     {
+        [Inject] protected ToDoStateContainer ToDoStateContainer { get; init; }
+        [Inject] protected HttpClient HttpClient { get; init; }
         [Inject] private NavigationManager NavigationManager { get; init; }
         [Inject] private IJSRuntime Js { get; init; }
 
@@ -30,8 +36,13 @@ namespace Couple.Client.Pages.ToDo.Components
         private async Task OnCheckboxToggle(Guid id, ToDoInnerViewModel toDo)
         {
             toDo.IsCompleted = !toDo.IsCompleted;
-            var toUpdate = ToDos.Single(x => x.Id == id);
-            await Js.InvokeVoidAsync("updateToDo", toUpdate);
+            var toPersist = ToDoAdapter.ToModel(ToDos.Single(x => x.Id == id));
+
+            await Js.InvokeVoidAsync("updateToDo", toPersist);
+            ToDoStateContainer.ToDos = await Js.InvokeAsync<List<ToDoModel>>("getAllToDos");
+
+            var toUpdate = ToDoAdapter.ToUpdateDto(toPersist);
+            await HttpClient.PutAsJsonAsync("api/ToDos", toUpdate);
         }
     }
 }
