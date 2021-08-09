95c48e91ab1f80867cf164546e0b8ce1c6a1cb68
Yong Zhi Yuan <yongzhiyuan@u.nus.edu>
2021-08-09 11:09:00 +0800

    Create API

---
+++ Api/Features/ToDo/CompleteToDoFunction.cs
@@ -0,0 +1,84 @@
+using Couple.Api.Data;
+using Couple.Api.Infrastructure;
+using Couple.Shared.Model.Change;
+using Couple.Shared.Model.ToDo;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Couple.Api.Features.ToDo
+{
+    public class CompleteToDoFunction
+    {
+        private readonly ChangeContext _context;
+        private readonly IDateTimeService _dateTimeService;
+        private readonly ICurrentUserService _currentUserService;
+
+        public CompleteToDoFunction(ChangeContext context,
+            IDateTimeService dateTimeService,
+            ICurrentUserService currentUserService)
+        {
+            _context = context;
+            _dateTimeService = dateTimeService;
+            _currentUserService = currentUserService;
+        }
+
+        [FunctionName("CompleteToDoFunction")]
+        public async Task<ActionResult> CompleteToDo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "ToDos/Complete")]
+            HttpRequest req,
+            ILogger log)
+        {
+            var form = await req.GetJsonBody<CompleteToDoDto, Validator>();
+
+            if (!form.IsValid)
+            {
+                log.LogWarning("{ErrorMessage}", form.ErrorMessage());
+                return form.ToBadRequest();
+            }
+
+            if (_currentUserService.PartnerId == null)
+            {
+                return new BadRequestResult();
+            }
+
+            var toCreate = new Model.Change
+            {
+                Id = Guid.NewGuid(),
+                Function = Function.Create,
+                DataType = DataType.CompletedToDo,
+                UserId = _currentUserService.PartnerId,
+                Timestamp = _dateTimeService.Now,
+                Content = form.Json,
+            };
+
+            _context
+                .Changes
+                .Add(toCreate);
+            await _context.SaveChangesAsync();
+
+            return new OkResult();
+        }
+
+        private class Validator : AbstractValidator<CompleteToDoDto>
+        {
+            public Validator()
+            {
+                RuleFor(dto => dto.Id).NotEmpty();
+                RuleFor(dto => dto.Name).NotNull();
+                RuleFor(dto => dto.For).NotNull();
+                RuleFor(dto => dto.ToDos).NotNull();
+                RuleForEach(dto => dto.ToDos)
+                    .ChildRules(toDos =>
+                        toDos.RuleFor(toDo => toDo.Content).NotEmpty());
+                RuleFor(dto => dto.CreatedOn).NotEmpty();
+                RuleFor(dto => dto.CompletedOn).NotEmpty();
+            }
+        }
+    }
+}
--- Shared/Model/Change/DataType.cs
+++ Shared/Model/Change/DataType.cs
@@ -4,5 +4,6 @@
     {
         Calendar,
         ToDo,
+        CompletedToDo,
     }
 }
