7f1fb0259b3ed5002e7f1a5a851d0f8a9fc468a2
Yong Zhi Yuan <yongzhiyuan@u.nus.edu>
2021-08-22 08:36:00 +0800

    CreateUpdateToDoStateContainer: Remove unused code

--- Client/States/ToDo/CreateUpdateToDoStateContainer.cs
+++ Client/States/ToDo/CreateUpdateToDoStateContainer.cs
@@ -8,40 +8,19 @@ using System.Linq;
 
 namespace Couple.Client.States.ToDo
 {
-    public class CreateUpdateToDoStateContainer : Notifier
+    public class CreateUpdateToDoStateContainer
     {
         public Guid Id { get; private set; }
-        private string _name;
-        private For _for;
+        public string Name { get; set; }
+        public For For { get; set; }
         private List<CreateUpdateInnerViewModel> _toDos;
         public DateTime CreatedOn { get; private set; }
-
-        public string Name
-        {
-            get => _name;
-            set
-            {
-                _name = value;
-                NotifyStateChanged();
-            }
-        }
-
-        public For For
-        {
-            get => _for;
-            set
-            {
-                _for = value;
-                NotifyStateChanged();
-            }
-        }
 
         public IReadOnlyList<IReadOnlyInnerViewModel> ToDos => _toDos;
 
         public void AddToDo(string content, bool isCompleted)
         {
             _toDos.Add(new() {Content = content, IsCompleted = isCompleted,});
-            NotifyStateChanged();
         }
 
         public void TrimToDos() => _toDos.RemoveAll(toDo => !toDo.Content.Any());
@@ -49,21 +28,20 @@ namespace Couple.Client.States.ToDo
         public void SetContent(int index, string content)
         {
             _toDos[index].Content = content;
-            NotifyStateChanged();
         }
 
         public void Initialize(string name, For @for, IEnumerable<ToDoInnerModel> toDos)
         {
-            _name = name;
-            _for = @for;
+            Name = name;
+            For = @for;
             _toDos = ToDoAdapter.ToCreateUpdateInnerViewModel(toDos);
         }
 
         public void Initialize(Guid id, string name, For @for, IEnumerable<ToDoInnerModel> toDos, DateTime createdOn)
         {
             Id = id;
-            _name = name;
-            _for = @for;
+            Name = name;
+            For = @for;
             _toDos = ToDoAdapter.ToCreateUpdateInnerViewModel(toDos);
             CreatedOn = createdOn;
         }
@@ -71,8 +49,8 @@ namespace Couple.Client.States.ToDo
         public void Reset()
         {
             Id = Guid.Empty;
-            _name = null;
-            _for = For.Him;
+            Name = null;
+            For = For.Him;
             _toDos = new();
             CreatedOn = DateTime.UnixEpoch;
         }
