2f5dfa4bef0277ed5af1f1499f6d280b08c44b82
Yong Zhi Yuan <yongzhiyuan@u.nus.edu>
2021-08-23 20:55:57 +0800

    Add Import & Export functionality

---
+++ Client/Pages/Settings/Settings.razor.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.JSInterop;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Couple.Client.Pages.Settings
+{
+    public partial class Settings
+    {
+        [Inject] private IJSRuntime Js { get; init; }
+
+        private async Task OnImportSelected(InputFileChangeEventArgs e)
+        {
+            if (e.File.ContentType != "application/json")
+            {
+                return;
+            }
+
+            var json =
+                await new StreamReader(e.File.OpenReadStream()).ReadToEndAsync();
+            await Js.InvokeVoidAsync("importDatabase", json);
+        }
+
+        private async Task OnExportSelected()
+        {
+            var json = await Js.InvokeAsync<string>("exportDatabase");
+            var bytes = System.Text.Encoding.UTF8.GetBytes(json);
+            await Js.InvokeAsync<object>(
+                "saveAsFile",
+                "couple.json",
+                Convert.ToBase64String(bytes));
+        }
+    }
+}
