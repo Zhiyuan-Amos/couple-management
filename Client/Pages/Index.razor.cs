a54939d13391469e8372fd6828819667c7083b85
Yong Zhi Yuan <yongzhiyuan@u.nus.edu>
2021-12-17 21:11:21 +0800

    Bugfix DoneStateContainer not initialized during Update from Index page

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -1,4 +1,5 @@
 using Couple.Client.Model.Image;
+using Couple.Client.States.Done;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
@@ -8,11 +9,20 @@ public partial class Index
 {
     [Inject] private NavigationManager NavigationManager { get; init; }
     [Inject] private IJSRuntime Js { get; init; }
+    [Inject] private DoneStateContainer DoneStateContainer { get; init; }
     private List<ImageModel> FavouriteImages { get; set; } = new();
 
     protected override async Task OnInitializedAsync()
     {
         FavouriteImages = await Js.InvokeAsync<List<ImageModel>>("getFavouriteImages");
+        if (FavouriteImages.Count > 0 && DoneStateContainer.GetDateToItems().Count == 0)
+        {
+            var toStore = FavouriteImages
+                .GroupBy(image => DateOnly.FromDateTime(image.TakenOn))
+                .ToDictionary(grouping => grouping.Key,
+                    grouping => (IReadOnlyList<object>)grouping.Select(image => (object)image).ToList());
+            DoneStateContainer.SetDateToItems(toStore);
+        }
     }
 
     private void EditImage(ImageModel selectedImage)
