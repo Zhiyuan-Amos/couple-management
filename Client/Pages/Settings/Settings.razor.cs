069c2a3bff8a9da9f7c9e3b8b6f86439acdb78cb
Yong Zhi Yuan <yongzhiyuan@u.nus.edu>
2021-12-29 17:56:21 +0800

    Add "logging" for import

--- Client/Pages/Settings/Settings.razor.cs
+++ Client/Pages/Settings/Settings.razor.cs
@@ -8,18 +8,37 @@ public partial class Settings
 {
     [Inject] private NavigationManager NavigationManager { get; init; }
     [Inject] private IJSRuntime Js { get; init; }
+    private string ProgressMessage { get; set; } = "";
 
     private async Task OnImportSelected(InputFileChangeEventArgs e)
     {
         if (e.File.ContentType != "application/json")
         {
+            ProgressMessage = "Invalid file";
+            StateHasChanged();
             return;
         }
 
-        await Js.InvokeVoidAsync("clearDatabase");
-        var json =
-            await new StreamReader(e.File.OpenReadStream(512000000)).ReadToEndAsync();
-        await Js.InvokeVoidAsync("importDatabase", json);
+        try
+        {
+            ProgressMessage = "Valid file";
+            StateHasChanged();
+            await Js.InvokeVoidAsync("clearDatabase");
+            ProgressMessage = "Database cleared";
+            StateHasChanged();
+            var json =
+                await new StreamReader(e.File.OpenReadStream(512000000)).ReadToEndAsync();
+            ProgressMessage = "Json read into memory";
+            StateHasChanged();
+            await Js.InvokeVoidAsync("importDatabase", json);
+            ProgressMessage = "Database imported";
+            StateHasChanged();
+        }
+        catch (Exception ex)
+        {
+            ProgressMessage = ex.Message;
+            StateHasChanged();
+        }
     }
 
     private async Task OnExportSelected() => await Js.InvokeAsync<object>("exportDatabaseAsFile", "couple.json");
