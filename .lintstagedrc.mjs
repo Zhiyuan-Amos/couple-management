acb57ce5170a3ee5d7a8ed49afee03f95935c328
Yong Zhi Yuan <yongzhiyuan@u.nus.edu>
2022-01-03 14:28:41 +0800

    Replace custom git hook with husky

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,12 @@
+﻿import micromatch from 'micromatch'
+
+export default {
+    '*.+(csproj|cs|razor|html|css|js|json)': (files) => {
+        const match = micromatch.not(files, ['package.json', 'package-lock.json'])
+        if (match.length === 0) {
+            return ""
+        }
+
+        return `jb cleanupcode Couple.sln --include=${match.join(";")}`
+    }
+}
