f3ed99125430e5f52a86783b7c51455d2e2e638e
Yongkiat Goh <yongkiat.goh@shopee.com>
2022-01-17 23:42:07 +0800

    Change App Service Plan pricing tier and kind

--- terraform/terraform-create-resource/functionapp.tf
+++ terraform/terraform-create-resource/functionapp.tf
@@ -2,10 +2,11 @@ resource "azurerm_app_service_plan" "asp" {
   name                = "${var.prefix}-service-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  kind                = "FunctionApp"
 
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = "Dynamic"
+    size = "Y1"
   }
 }
 
