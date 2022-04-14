27d05711203616405cef5b904c7089891dcc27cb
Yong Zhi Yuan <yongzhiyuan@u.nus.edu>
2022-04-14 14:28:51 +0800

    Simplify auth

--- Client/Shared/MainLayout.razor.cs
+++ Client/Shared/MainLayout.razor.cs
@@ -8,20 +8,7 @@ public partial class MainLayout
 {
     private static bool s_isDataLoaded;
     [CascadingParameter] private Task<AuthenticationState> AuthenticationStateTask { get; set; } = default!;
-    [Inject] private NavigationManager NavigationManager { get; set; } = default!;
     [Inject] private Synchronizer Synchronizer { get; init; } = default!;
-
-    protected override async Task OnInitializedAsync()
-    {
-        // Should have done https://docs.microsoft.com/en-us/aspnet/core/blazor/security/webassembly/?view=aspnetcore-6.0#require-authorization-for-the-entire-app,
-        // but that doesn't work
-        var authenticationState = await AuthenticationStateTask;
-        if (authenticationState.User.Identity is null || !authenticationState.User.Identity.IsAuthenticated)
-        {
-            NavigationManager.NavigateTo(
-                $"authentication/login?returnUrl={Uri.EscapeDataString(NavigationManager.Uri)}");
-        }
-    }
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
