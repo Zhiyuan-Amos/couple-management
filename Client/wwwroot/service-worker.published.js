634f961753f52f6b988a2424beefe37acbbcded2
Yong Zhi Yuan <yongzhiyuan@u.nus.edu>
2022-04-30 00:40:16 +0800

    Comment caching temporarily

--- Client/wwwroot/service-worker.published.js
+++ Client/wwwroot/service-worker.published.js
@@ -15,11 +15,11 @@ async function onInstall(event) {
     console.info('Service worker: Install');
 
     // Fetch and cache all matching items from the assets manifest
-    const assetsRequests = self.assetsManifest.assets
-        .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
-        .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
-        .map(asset => new Request(asset.url, { integrity: asset.hash }));
-    await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+    // const assetsRequests = self.assetsManifest.assets
+    //     .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
+    //     .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
+    //     .map(asset => new Request(asset.url, { integrity: asset.hash }));
+    // await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
 }
 
 async function onActivate(event) {
