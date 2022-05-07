992b1d90e785fe5949914c704008edff6f6e33d7
Yong Zhi Yuan <yongzhiyuan@u.nus.edu>
2022-05-07 16:18:49 +0800

    Another dummy commit

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -1,18 +1,28 @@
 using Couple.Client.Model.Done;
 using Couple.Client.Model.Image;
+using Couple.Client.Services.Settings;
 using Couple.Client.Services.Synchronizer;
 using Couple.Client.States.Done;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.JSInterop;
 
 namespace Couple.Client.Pages;
 
 public partial class Index
 {
     private static bool s_isDataLoaded;
+    private static bool s_isDataSynchronized;
     [Inject] private DbContextProvider DbContextProvider { get; init; } = default!;
     [Inject] private NavigationManager NavigationManager { get; init; } = default!;
     [Inject] private DoneStateContainer DoneStateContainer { get; init; } = default!;
+    
+    [CascadingParameter] private Task<AuthenticationState> AuthenticationStateTask { get; set; } = default!;
+    [Inject] private Synchronizer Synchronizer { get; init; } = default!;
+    [Inject] private IJSRuntime Js { get; init; } = default!;
+    [Inject] private IOptions<AuthenticationOptions> AuthOptions { get; init; } = default!;
 
     private List<IReadOnlyImageModel> FavouriteImages =>
         DoneStateContainer.FavouriteImages
@@ -51,6 +61,21 @@ public partial class Index
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
+        var ao = AuthOptions.Value;
+        // This seems necessary as https://docs.microsoft.com/en-us/azure/active-directory/develop/msal-net-aad-b2c-considerations#acquire-a-token-to-apply-a-policy doesn't work
+        Js.InvokeVoidAsync("login", ao.ClientId, ao.Authority, ao.KnownAuthority, ao.RedirectUri)
+            .AsTask()
+            .ContinueWith(_ => AuthenticationStateTask) // Ensures that background API calls are only done after authentication state has been set
+            .ContinueWith(_ =>
+            {
+                if (!s_isDataSynchronized)
+                {
+                    s_isDataSynchronized = true;
+                    return Synchronizer.SynchronizeAsync();
+                }
+
+                return Task.CompletedTask;
+            });
     }
 
     private void EditImage(IReadOnlyImageModel selectedImage) =>
--- Client/Shared/MainLayout.razor.cs
+++ Client/Shared/MainLayout.razor.cs
@@ -1,36 +1,5 @@
-using Couple.Client.Services.Settings;
-using Couple.Client.Services.Synchronizer;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.Extensions.Options;
-using Microsoft.JSInterop;
-
 namespace Couple.Client.Shared;
 
 public partial class MainLayout
 {
-    private static bool s_isDataLoaded;
-    [CascadingParameter] private Task<AuthenticationState> AuthenticationStateTask { get; set; } = default!;
-    [Inject] private Synchronizer Synchronizer { get; init; } = default!;
-    [Inject] private IJSRuntime Js { get; init; } = default!;
-    [Inject] private IOptions<AuthenticationOptions> AuthOptions { get; init; } = default!;
-
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        var ao = AuthOptions.Value;
-        // This seems necessary as https://docs.microsoft.com/en-us/azure/active-directory/develop/msal-net-aad-b2c-considerations#acquire-a-token-to-apply-a-policy doesn't work
-        Js.InvokeVoidAsync("login", ao.ClientId, ao.Authority, ao.KnownAuthority, ao.RedirectUri)
-            .AsTask()
-            .ContinueWith(_ => AuthenticationStateTask) // Ensures that background API calls are only done after authentication state has been set
-            .ContinueWith(_ =>
-            {
-                if (!s_isDataLoaded)
-                {
-                    s_isDataLoaded = true;
-                    return Synchronizer.SynchronizeAsync();
-                }
-
-                return Task.CompletedTask;
-            });
-    }
 }
